ef445da840d97af620b2a3fd1aff10529dbdf0f5
sithydev :v <109386708+sithydev@users.noreply.github.com>
2023-09-05 19:58:33 +0200

    Create check_password.php

---
+++ gdonline/check_password.php
@@ -0,0 +1,21 @@
+<?php
+// Define the correct password
+$correctPassword = "i love black oily men";
+
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get the password submitted in the form
+    $enteredPassword = $_POST["password"];
+
+    // Check if the entered password matches the correct password
+    if ($enteredPassword === $correctPassword) {
+        // Password is correct, grant access
+        echo "<h1>Access Granted!</h1>";
+        echo "<p>You can now access the protected content.</p>";
+    } else {
+        // Password is incorrect, deny access
+        echo "<h1>Access Denied!</h1>";
+        echo "<p>The entered password is incorrect. Please try again.</p>";
+    }
+}
+?>
