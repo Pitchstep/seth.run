140a2aa1d8f1feeaff6986b1de0cd7b73af5a86b
sithydev :v <109386708+sithydev@users.noreply.github.com>
2024-04-14 18:51:29 +0200

    Update script.js

--- rng/script.js
+++ rng/script.js
@@ -20,11 +20,19 @@ function executeCommand(input) {
 
 // Function to play sound
 function playSound(soundId) {
-  var sound = document.getElementById(soundId);
-  if (sound) {
-    sound.currentTime = 0; // Reset sound to beginning
-    sound.play();
-  }
+    var sound = document.getElementById(soundId);
+    if (sound) {
+        sound.currentTime = 0; // Reset sound to beginning
+        sound.play();
+    }
+}
+
+// Function to change the volume of all audio elements
+function changeVolume(volume) {
+    var audioElements = document.querySelectorAll('audio');
+    audioElements.forEach(function(audio) {
+        audio.volume = volume;
+    });
 }
 
 // listen for console input
@@ -63,36 +71,40 @@ document.getElementById('generate-btn').addEventListener('click', function() {
     var rarity;
     var rarityColor;
     if (randomNumber <= 50) {
-      rarity = 'Pray to RNGesus';
-      rarityColor = '#FA8CE6';
-      playSound('rngesus-sound');
+        rarity = 'RNGesus Incarnate';
+        rarityColor = '#FA8CE6';
+        playSound('rngesus-sound');
     } else if (randomNumber <= 500) {
         rarity = 'Mythical';
         rarityColor = '#FF69B4';
         playSound('mythical-sound');
     } else if (randomNumber <= 1000) {
-      rarity = 'Legendary';
-      rarityColor = '#FFD700'; // Gold
-      playSound('legendary-sound');
+        rarity = 'Legendary';
+        rarityColor = '#FFD700'; // Gold
+        playSound('legendary-sound');
     } else if (randomNumber <= 2500) {
-      rarity = 'Epic';
-      rarityColor = '#800080'; // Purple
-      playSound('epic-sound');
+        rarity = 'Epic';
+        rarityColor = '#800080'; // Purple
+        playSound('epic-sound');
     } else if (randomNumber <= 5000) {
-      rarity = 'Rare';
-      rarityColor = '#0000FF'; // Blue
-      playSound('rare-sound');
+        rarity = 'Rare';
+        rarityColor = '#0000FF'; // Blue
+        playSound('rare-sound');
     } else {
-      rarity = 'Common';
-      rarityColor = '#FFFFFF'; // White
-      playSound('common-sound');
+        rarity = 'Common';
+        rarityColor = '#FFFFFF'; // White
+        playSound('common-sound');
     }
     
     // update text with rarity after a delay
     setTimeout(function() {
-      var rarityElement = document.createElement('div');
-      rarityElement.innerHTML = '<b class="pop-up" style="text-transform: uppercase; font-style: 500; font-size: 14px; color: ' + rarityColor + ';">' + rarity + '</b>';
-      resultElement.appendChild(rarityElement);
+        var rarityElement = document.createElement('div');
+        rarityElement.innerHTML = '<b class="pop-up" style="text-transform: uppercase; font-style: 500; font-size: 14px; color: ' + rarityColor + ';">' + rarity + '</b>';
+        resultElement.appendChild(rarityElement);
     }, 500); // .5 sec delay for rarity popup
 });
-// update
+
+// Set the volume of all audio elements to 0.4 when the page loads
+window.addEventListener('load', function() {
+    changeVolume(0.05);
+});
