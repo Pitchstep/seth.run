59930f1c8dee94f682cb536576007ddf77d3f667
Pitchstep <109386708+Pitchstep@users.noreply.github.com>
2024-04-22 13:05:26 +0000

    actually fixed firebase

--- rng/code/script.mjs
+++ rng/code/script.mjs
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "./firebase/app";
-import { getAnalytics } from "./firebase/analytics";
+import { initializeApp } from "https://seth.run/node_modules/firebase/app";
+import { getAnalytics } from "https://seth.run/node_modules/firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
