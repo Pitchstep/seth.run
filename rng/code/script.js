60e808310dd147d05b84b57b1c3bf1ad689797bf
Pitchstep <109386708+Pitchstep@users.noreply.github.com>
2024-05-20 16:30:11 +0200

    Update script.js

--- rng/code/script.js
+++ rng/code/script.js
@@ -25,7 +25,7 @@ document.getElementById('generate-btn').addEventListener('click', function() {
     buttonEnabled = false;
     setTimeout(function() {
         buttonEnabled = true; // enable the button after 3 seconds
-    }, 600);
+    }, 1200);
 
     // RNG!!!!!
     var randomNumber = Math.floor(Math.random() * 10000) + 1;
