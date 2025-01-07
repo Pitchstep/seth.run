7dc2df3c810048fbbf77772bfa72b13296c75ed0
Pitchstep <109386708+Pitchstep@users.noreply.github.com>
2025-01-07 11:55:15 +0100

    Update script.js

--- rng/code/script.js
+++ rng/code/script.js
@@ -1,1 +1,106 @@
-function _0xbc80(){var _0x1b5e11=['random','#ffabe0','play','appendChild','querySelectorAll','textContent','addEventListener','floor','243078cYRKrf','548344bELicJ','Common','#FFFFFF','#800080','pop-up','#FF69B4','RNGesus\x20Incarnate','2XHvNwI','rare-sound','43730hUAOpK','</b>','mystical-sound','#FFD700','original-rarity-sound','common-sound','ultra-sound','createElement','result','Too\x20Common?','2313262vuKgvW','Mythic','audio','#ff0000','forEach','Not\x20Found','<b\x20class=\x22pop-up\x22\x20style=\x22text-transform:\x20uppercase;\x20font-style:\x20680;\x20font-size:\x2014px;\x20color:\x20','classList','div','2034tqbPdU','volume','Glorious','epic-sound','Epic','20ekzTha','#0000FF','1546323fUewts','#FA8CE6','#fffb00','legendary-sound','Triple\x20Seven','Rare','super-sound','344952oesIto','2233072RTqyJQ','click',';\x22>','getElementById'];_0xbc80=function(){return _0x1b5e11;};return _0xbc80();}function _0x1f57(_0x2e9ade,_0x331000){var _0xbc800d=_0xbc80();return _0x1f57=function(_0x1f57ca,_0x255d05){_0x1f57ca=_0x1f57ca-0x1b0;var _0x102d1c=_0xbc800d[_0x1f57ca];return _0x102d1c;},_0x1f57(_0x2e9ade,_0x331000);}var _0x5f5247=_0x1f57;(function(_0x4541e8,_0x58c714){var _0x3d60a4=_0x1f57,_0x23e60e=_0x4541e8();while(!![]){try{var _0x16d3ee=parseInt(_0x3d60a4(0x1e6))/0x1+parseInt(_0x3d60a4(0x1c3))/0x2*(-parseInt(_0x3d60a4(0x1df))/0x3)+-parseInt(_0x3d60a4(0x1bc))/0x4*(parseInt(_0x3d60a4(0x1dd))/0x5)+-parseInt(_0x3d60a4(0x1bb))/0x6+parseInt(_0x3d60a4(0x1cf))/0x7+-parseInt(_0x3d60a4(0x1e7))/0x8+parseInt(_0x3d60a4(0x1d8))/0x9*(parseInt(_0x3d60a4(0x1c5))/0xa);if(_0x16d3ee===_0x58c714)break;else _0x23e60e['push'](_0x23e60e['shift']());}catch(_0x5f5a64){_0x23e60e['push'](_0x23e60e['shift']());}}}(_0xbc80,0x446dc));var buttonEnabled=!![];function playSound(_0x2615f0){var _0x24185d=_0x1f57,_0x76d4ba=document[_0x24185d(0x1b2)](_0x2615f0);_0x76d4ba&&(_0x76d4ba['currentTime']=0x0,_0x76d4ba[_0x24185d(0x1b5)]());}function changeVolume(_0x4bedf7){var _0x59d3ed=_0x1f57,_0x2c5ed5=document[_0x59d3ed(0x1b7)](_0x59d3ed(0x1d1));_0x2c5ed5[_0x59d3ed(0x1d3)](function(_0x198900){var _0x268f4f=_0x59d3ed;_0x198900[_0x268f4f(0x1d9)]=_0x4bedf7;});}document[_0x5f5247(0x1b2)]('generate-btn')[_0x5f5247(0x1b9)](_0x5f5247(0x1b0),function(){var _0x3c9319=_0x5f5247;if(!buttonEnabled)return;buttonEnabled=![],setTimeout(function(){buttonEnabled=!![];},0x4b0);var _0x5c8a18=Math[_0x3c9319(0x1ba)](Math['random']()*0x2710)+0x1;_0x5c8a18!==null?_0x5c8a18:Math[_0x3c9319(0x1ba)](Math[_0x3c9319(0x1b3)]()*0x2710)+0x1;var _0x91e4af=document[_0x3c9319(0x1b2)](_0x3c9319(0x1cd));_0x91e4af['innerHTML']='';var _0x5a8272=document[_0x3c9319(0x1cc)]('b');_0x5a8272[_0x3c9319(0x1d6)]['add'](_0x3c9319(0x1c0)),_0x5a8272[_0x3c9319(0x1b8)]=_0x5c8a18,_0x91e4af[_0x3c9319(0x1b6)](_0x5a8272);var _0x3694df,_0x51ede7,_0x20cdd1=![];if(_0x5c8a18===0xd)_0x3694df='Unlucky',_0x51ede7=_0x3c9319(0x1d2),playSound('unlucky-sound'),_0x20cdd1=!![];else{if(_0x5c8a18===0x194)_0x3694df=_0x3c9319(0x1d4),_0x51ede7=_0x3c9319(0x1b4),playSound('404-sound'),_0x20cdd1=!![];else{if(_0x5c8a18===0x309)_0x3694df=_0x3c9319(0x1e3),_0x51ede7=_0x3c9319(0x1e1),playSound('luckysevens-sound'),_0x20cdd1=!![];else{if(_0x5c8a18<=0x1)_0x3694df=_0x3c9319(0x1c2),_0x51ede7='#FFD700',playSound(_0x3c9319(0x1cb));else{if(_0x5c8a18<=0x64)_0x3694df=_0x3c9319(0x1da),_0x51ede7=_0x3c9319(0x1e0),playSound(_0x3c9319(0x1e5));else{if(_0x5c8a18<=0x12c)_0x3694df=_0x3c9319(0x1d0),_0x51ede7=_0x3c9319(0x1c1),playSound(_0x3c9319(0x1c7));else{if(_0x5c8a18<=0x1f4)_0x3694df='Legendary',_0x51ede7=_0x3c9319(0x1c8),playSound(_0x3c9319(0x1e2));else{if(_0x5c8a18<=0x9c4)_0x3694df=_0x3c9319(0x1dc),_0x51ede7=_0x3c9319(0x1bf),playSound(_0x3c9319(0x1db));else{if(_0x5c8a18<=0x1388)_0x3694df=_0x3c9319(0x1e4),_0x51ede7=_0x3c9319(0x1de),playSound(_0x3c9319(0x1c4));else _0x5c8a18>=0x2711?(_0x3694df=_0x3c9319(0x1ce),_0x51ede7=_0x3c9319(0x1be),playSound(_0x3c9319(0x1ca))):(_0x3694df=_0x3c9319(0x1bd),_0x51ede7=_0x3c9319(0x1be),playSound('common-sound'));}}}}}}}}!_0x20cdd1&&playSound(_0x3c9319(0x1c9)),setTimeout(function(){var _0x2e9393=_0x3c9319,_0x1729b2=document['createElement'](_0x2e9393(0x1d7));_0x1729b2['innerHTML']=_0x2e9393(0x1d5)+_0x51ede7+_0x2e9393(0x1b1)+_0x3694df+_0x2e9393(0x1c6),_0x91e4af['appendChild'](_0x1729b2);},0xc8);});
+var buttonEnabled = true
+function playSound(_0x2615f0) {
+  var _0x76d4ba = document.getElementById(_0x2615f0)
+  _0x76d4ba && ((_0x76d4ba.currentTime = 0), _0x76d4ba.play())
+}
+function changeVolume(_0x4bedf7) {
+  var _0x2c5ed5 = document.querySelectorAll('audio')
+  _0x2c5ed5.forEach(function (_0x198900) {
+    _0x198900.volume = _0x4bedf7
+  })
+}
+document.getElementById('generate-btn').addEventListener('click', function () {
+  if (!buttonEnabled) {
+    return
+  }
+  buttonEnabled = false
+  setTimeout(function () {
+    buttonEnabled = true
+  }, 1)
+  var _0x5c8a18 = Math.floor(Math.random() * 10000) + 1
+  _0x5c8a18 !== null ? _0x5c8a18 : Math.floor(Math.random() * 10000) + 1
+  var _0x91e4af = document.getElementById('result')
+  _0x91e4af.innerHTML = ''
+  var _0x5a8272 = document.createElement('b')
+  _0x5a8272.classList.add('pop-up')
+  _0x5a8272.textContent = _0x5c8a18
+  _0x91e4af.appendChild(_0x5a8272)
+  var _0x3694df,
+    _0x51ede7,
+    _0x20cdd1 = false
+  if (_0x5c8a18 === 13) {
+    _0x3694df = 'Unlucky'
+    _0x51ede7 = '#ff0000'
+    playSound('unlucky-sound')
+    _0x20cdd1 = true
+  } else {
+    if (_0x5c8a18 === 404) {
+      _0x3694df = 'Not Found'
+      _0x51ede7 = '#ffabe0'
+      playSound('404-sound')
+      _0x20cdd1 = true
+    } else {
+      if (_0x5c8a18 === 777) {
+        _0x3694df = 'Triple Seven'
+        _0x51ede7 = '#fffb00'
+        playSound('luckysevens-sound')
+        _0x20cdd1 = true
+      } else {
+        if (_0x5c8a18 <= 1) {
+          _0x3694df = 'RNGesus Incarnate'
+          _0x51ede7 = '#FFD700'
+          playSound('ultra-sound')
+        } else {
+          if (_0x5c8a18 <= 100) {
+            _0x3694df = 'Glorious'
+            _0x51ede7 = '#FA8CE6'
+            playSound('super-sound')
+          } else {
+            if (_0x5c8a18 <= 300) {
+              _0x3694df = 'Mythic'
+              _0x51ede7 = '#FF69B4'
+              playSound('mystical-sound')
+            } else {
+              if (_0x5c8a18 <= 500) {
+                _0x3694df = 'Legendary'
+                _0x51ede7 = '#FFD700'
+                playSound('legendary-sound')
+              } else {
+                if (_0x5c8a18 <= 2500) {
+                  _0x3694df = 'Epic'
+                  _0x51ede7 = '#800080'
+                  playSound('epic-sound')
+                } else {
+                  if (_0x5c8a18 <= 5000) {
+                    _0x3694df = 'Rare'
+                    _0x51ede7 = '#0000FF'
+                    playSound('rare-sound')
+                  } else {
+                    _0x5c8a18 >= 10001
+                      ? ((_0x3694df = 'Too Common?'),
+                        (_0x51ede7 = '#FFFFFF'),
+                        playSound('common-sound'))
+                      : ((_0x3694df = 'Common'),
+                        (_0x51ede7 = '#FFFFFF'),
+                        playSound('common-sound'))
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  !_0x20cdd1 && playSound('original-rarity-sound')
+  setTimeout(function () {
+    var _0x1729b2 = document.createElement('div')
+    _0x1729b2.innerHTML =
+      '<b class="pop-up" style="text-transform: uppercase; font-style: 680; font-size: 14px; color: ' +
+      _0x51ede7 +
+      ';">' +
+      _0x3694df +
+      '</b>'
+    _0x91e4af.appendChild(_0x1729b2)
+  }, 200)
+})
